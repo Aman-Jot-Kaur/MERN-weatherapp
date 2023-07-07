8d5262656a5ec58fa59cd6cd53a90a4661bec979
Amanjot-Kaur-Narang <amritanarang6@gmail.com>
2023-07-07 12:20:58 +0530

    design betterment

--- frontend/loginapp/src/components/Login.js
+++ frontend/loginapp/src/components/Login.js
@@ -2,25 +2,38 @@ import React from 'react'
 import "./Login.css"
 function Login() {
   return (
-    <div >
+    <div>
           <div className="form">
-      <form action="http://localhost:3000/signup" method="post" >
-        <div className="input-container">
+      <form action="http://localhost:3000/signup" method="post" style={{  backdropFilter: `blur(18px)`,
+         color:"black"}} >
+        <div className="input-container" style={{fontSize:"large"}}>
           <label>Username </label>
           <input type="text" name="username" required />
         
         </div>
-        <div className="input-container">
+        <div className="input-container" style={{fontSize:"large"}}>
           <label>Password </label>
           <input type="password" name="password" required />
          
         </div>
         <div className="button-container">
-          <input type="submit" />
+          <input style={{ padding: "12px",
+            border: "none",
+            borderRadius: "12px",
+            backgroundColor: "#304356",
+            color: "white",cursor:"pointer",
+            fontSize: "large"}} type="submit" />
           
         </div>
       </form>
-      <button style={{marginLeft:"10px",height:"60px"}} href="/checkuser">login instead</button>
+      <br></br>
+      <button style={{ padding: "12px",
+            border: "none",
+            borderRadius: "12px",
+            backgroundColor: "#304356",
+            color: "white",
+            fontSize: "large",
+            cursor:"pointer"}} href="/checkuser">login instead</button>
     </div>
     </div>
   )
--- frontend/loginapp/src/components/Weatherapp.js
+++ frontend/loginapp/src/components/Weatherapp.js
@@ -44,8 +44,7 @@ function Weather() {
         justifyContent: "center",
         fontFamily: "cursive",  
         fontSize: "large",
-        background: `url("https://images.unsplash.com/photo-1580193769210-b8d1c049a7d9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1474&q=80")`,
-        backgroundRepeat: "no-repeat",
+      
        
       }}
     >
